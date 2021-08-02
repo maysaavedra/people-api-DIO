cc9795287cbdd7652c1a60c2c42b56e367730874
maysaavedra <mayara.saavedra1101@gmail.com>
2021-08-02 12:25:09 -0300

    Initial Tests

---
+++ src/main/java/com/digitalinnovation/MpersonaAPI/MpersonaApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.MpersonaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpersonaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpersonaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/MpersonaAPI/MpersonaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.MpersonaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpersonaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
